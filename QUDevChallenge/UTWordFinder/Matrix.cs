8ec33d520b5980531e2f1b4dc3e2585470265d8d
DAVID OMAR ROMO MARTINEZ <34380349+DORMMX@users.noreply.github.com>
2024-06-21 07:55:26 -0600

    Task5: Add Test Case and Fix Version Testing

--- QUDevChallenge/UTWordFinder/Matrix.cs
+++ QUDevChallenge/UTWordFinder/Matrix.cs
@@ -1,14 +1,64 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QUDevChallenge.WordFinder;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UTWordFinder
 {
     [TestClass]
     public class Matrix
     {
+        //Mock
+        private readonly WordFinder _wordFinder;
+        public Matrix()
+        {
+            List<string> matrix =
+            [
+                "abcdc",
+                "fgwio",
+                "chill",
+                "pqnsd",
+                "uvdxy"
+            ];
+            _wordFinder = new(matrix);
+        }
+
         [TestMethod]
-        public void FindWord()
-        {          
-            Assert.IsTrue(1 == 1);
+        public void FindAWord()
+        {
+            List<string> words = [
+                "cold"               
+            ];
+
+            var result = _wordFinder.Find(words);
+
+            Assert.IsTrue(result.Count() == 1);
+        }
+
+        [TestMethod]
+        public void FindTwoWords()
+        {
+            List<string> words = [
+                "cold",
+                "chill"
+            ];
+
+            var result = _wordFinder.Find(words);
+
+            Assert.IsTrue(result.Count() == 2);
+        }
+
+        [TestMethod]
+        public void EmptySearch()
+        {
+            List<string> words = [
+                "cole",
+                "chilt"
+            ];
+
+            var result = _wordFinder.Find(words);
+
+            Assert.IsTrue(condition: !result.Any());
         }
     }
 }
